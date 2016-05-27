5b005e26ee77b3c770e5602754ce125f5d775df5
Francois Belanger <frank@smartbuildingapps.com>
2016-05-26 23:03:43 -0400

    Book validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   searchkick autocomplete: ['title']
+  validates_presence_of :title, :description, :image_url
 end
